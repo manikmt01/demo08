5856196366bc0983fdd51eb96584829b1737a7a7
Manik MT <165809938+ManikZellyo@users.noreply.github.com>
2024-06-18 22:17:52 +0600

    update 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: './src/**/*.{html,js}',
+  content: ['./*'],
   theme: {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
-    },
-    colors: {
-      blue: '#1fb6ff',
-      pink: '#ff49db',
-      orange: '#ff7849',
-      green: '#13ce66',
-      'gray-dark': '#273444',
-      gray: '#8492a6',
-      'gray-light': '#d3dce6',
-    },
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-    },
     extend: {
       spacing: {
         128: '32rem',
